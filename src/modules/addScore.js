621a0c9744e9231055381d123be8f0a81134e353
haliljon <110017001+haliljon@users.noreply.github.com>
2023-01-06 16:18:06 +0500

    connected to the API

---
+++ src/modules/addScore.js
@@ -0,0 +1,18 @@
+async function addScore(user, score, URL) {
+  await fetch(URL, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify({ user: user.value, score: score.value }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
+
+  user.value = '';
+  score.value = '';
+}
+
+export default addScore;
---
+++ src/modules/displayScore.js
@@ -0,0 +1,18 @@
+function displayScores(data) {
+  const tbody = document.querySelector('tbody');
+  tbody.textContent = '';
+  data.forEach((set) => {
+    // const tbody = document.querySelector('tbody');
+    const tr = document.createElement('tr');
+    tr.classList.add('list-item');
+    const tdName = document.createElement('td');
+    tdName.innerHTML = `${set.user}`;
+    const tdScore = document.createElement('td');
+    tdScore.innerHTML = `${set.score}`;
+    tr.appendChild(tdName);
+    tr.appendChild(tdScore);
+    tbody.appendChild(tr);
+  });
+}
+
+export default displayScores;
